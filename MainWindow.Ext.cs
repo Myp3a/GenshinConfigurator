9f9d10b6c2f64b8e5ce1a2ca2856a80d6a2ccfae
Myp3a <murza8965@gmail.com>
2023-12-18 22:19:29 +0300

    Graphics config UI

---
+++ MainWindow.Ext.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GenshinConfigurator.Enums;
+
+namespace GenshinConfiguratorWPF
+{
+    public partial class MainWindow
+    {
+        public int GraphicsPreset
+        {
+            get { if (Settings.Graphics.current_preset == -1) return 4; else return Settings.Graphics.current_preset; }
+            set { if (value == 4) Settings.Graphics.current_preset = -1; else Settings.Graphics.current_preset = value; } // reset presets
+        }
+
+        public int GraphicsFPSIndex
+        {
+            get => Settings.Graphics.Get(SettingsType.FPS) - 1;
+            set 
+            { Settings.Graphics.Change(SettingsType.FPS, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsVSync
+        {
+            get => Settings.Graphics.Get(SettingsType.VSync) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.VSync, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsRenderResolution
+        {
+            get => Settings.Graphics.Get(SettingsType.RenderResolution) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.RenderResolution, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsShadowQuality
+        {
+            get => Settings.Graphics.Get(SettingsType.ShadowQuality) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.ShadowQuality, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsVisualEffects
+        {
+            get => Settings.Graphics.Get(SettingsType.VisualEffects) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.VisualEffects, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsSFXQuality
+        {
+            get => Settings.Graphics.Get(SettingsType.SFXQuality) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.SFXQuality, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsEnvironmentDetail
+        {
+            get => Settings.Graphics.Get(SettingsType.EnvironmentDetail) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.EnvironmentDetail, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsAntialiasing
+        {
+            get => Settings.Graphics.Get(SettingsType.Antialiasing) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.Antialiasing, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsVolumetricFog
+        {
+            get => Settings.Graphics.Get(SettingsType.VolumetricFog) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.VolumetricFog, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsReflections
+        {
+            get => Settings.Graphics.Get(SettingsType.Reflections) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.Reflections, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsMotionBlur
+        {
+            get => Settings.Graphics.Get(SettingsType.MotionBlur) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.MotionBlur, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsBloom
+        {
+            get => Settings.Graphics.Get(SettingsType.Bloom) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.Bloom, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsCrowdDensity
+        {
+            get => Settings.Graphics.Get(SettingsType.CrowdDensity) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.CrowdDensity, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsSubsurfaceScattering
+        {
+            get => Settings.Graphics.Get(SettingsType.SubsurfaceScattering) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.SubsurfaceScattering, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsTeammateEffects
+        {
+            get => Settings.Graphics.Get(SettingsType.TeammateEffects) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.TeammateEffects, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public int GraphicsAnisotropicFiltering
+        {
+            get => Settings.Graphics.Get(SettingsType.AnisotropicFiltering) - 1;
+            set
+            {
+                Settings.Graphics.Change(SettingsType.AnisotropicFiltering, value + 1);
+                GraphicsPresetComboBox.SelectedIndex = 4;
+            }
+        }
+
+        public double GraphicsGamma
+        {
+            get => Settings.Graphics.gamma;
+            set => Settings.Graphics.gamma = value;
+        }
+
+        public int ResolutionWidth
+        {
+            get => Settings.Resolution.Get(ResolutionData.Width);
+            set => Settings.Resolution.Change(ResolutionData.Width, value);
+        }
+
+        public int ResolutionHeight
+        {
+            get => Settings.Resolution.Get(ResolutionData.Height);
+            set => Settings.Resolution.Change(ResolutionData.Height, value);
+        }
+
+        public bool ResolutionFullscreen
+        {
+            get => Convert.ToBoolean(Settings.Resolution.Get(ResolutionData.Fullscreen));
+            set => Settings.Resolution.Change(ResolutionData.Fullscreen, Convert.ToInt32(value));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using GenshinConfigurator;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GenshinConfiguratorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SettingsContainer Settings;
+        ResolutionSettings Resolution;
+        DispatcherTimer StatusTextTimer;
+        string status_text;
+
+        public string StatusText {
+            get { return status_text; }
+            private set { status_text = value; }
+        }
+        public MainWindow()
+        {
+            StatusTextTimer = new DispatcherTimer();
+            StatusTextTimer.Interval = TimeSpan.FromSeconds(10);
+            StatusTextTimer.Tick += new EventHandler(ResetStatusText);
+            Settings = new SettingsContainer();
+            Settings.Populate();
+            Resolution = Settings.Resolution;
+            ShowStatusText("Loaded config from registry.");
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public void SettingsApply()
+        {
+            Settings.Apply();
+        }
+
+        public void SettingsReset()
+        {
+
+        }
+
+        public void ShowStatusText(string text)
+        {
+            StatusText = text;
+            StatusTextTimer.Stop();
+            StatusTextTimer.Start();
+        }
+
+        public void ResetStatusText(object sender, EventArgs e)
+        {
+            StatusText = "Ready.";
+            StatusTextTimer.Stop();
+            StatusLabel.GetBindingExpression(Label.ContentProperty).UpdateTarget();
+        }
+    }
+}
